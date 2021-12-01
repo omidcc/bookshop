34cfee0ba487bca22da573a145181f9d6bda515e
omi <khairulomi@gmail.com>
2021-12-01 14:28:27 +0600

    primeng menu is added

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,16 @@
 import React from "react";
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-	const [data, setData] = React.useState(null);
+import 'primereact/resources/themes/bootstrap4-light-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
-  React.useEffect(() => {
-    fetch("/api/book/list")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
+import Header from "./components/Header";
+
+function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
     </div>
   );
 }
---
+++ client/src/components/Container.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Container(){
+    return (
+        <div>
+            <h1>This is Container</h1>
+        </div>
+    )
+}
+
+export default Container;
