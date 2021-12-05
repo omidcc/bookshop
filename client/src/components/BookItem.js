7df60927de04325082778fed900e1d7212098216
omi <khairulomi@gmail.com>
2021-12-05 21:07:44 +0600

    book item style updated

--- client/src/components/BookItem.js
+++ client/src/components/BookItem.js
@@ -1,12 +1,41 @@
 import React from "react"
+import { Card } from 'primereact/card';
+import { Rating } from 'primereact/rating';
+import { Button } from 'primereact/button';
 
 const BookItem =(props)=>{
     const book = props.book
-
+    const header = (
+        <div>
+            <div className="row p-2">
+                <div className="col-sm-12 col-md-6">
+                    <i className="pi pi-tag me-2"></i>
+                    <span>Horror, Drama</span>
+                </div>
+                <div className="col-sm-12 col-md-6">
+                    <span className="badge bg-success float-end ms-2">IN STOCK</span>
+                </div>
+            </div>
+            <img alt="Card" src={book.cover} style={{"width":"200px","height":"200px"}} className="m-2"/>
+        </div>
+    );
+    const footer = (
+        <div className="row">
+            <div className="col-sm-12 col-md-6">
+                <span className="fw-bold float-start">${parseInt(book.price)}<small className="text-muted">.{(book.price+'').replace(/^[-\d]+\./, '')}</small></span>
+            </div>
+            <div className="col-sm-12 col-md-6">
+                <Button label="Add to cart" className="p-button-info p-button-text p-0 float-end" icon="pi pi-shopping-cart" />
+            </div>
+        </div>
+    );
     return(
         
-        <div className="col-md-4">
-            <img src={book.cover} alt={book.title} style={{"maxWidth":"300px"}}/>
+        <div className="col-md-3 p-4">
+            <Card title={book.title} subTitle="" footer={footer} header={header}>
+                <p className="text-start">{book.description.substring(0,150)}.....</p>
+                <Rating value={4} stars={5} cancel={false} className="text-warning" tooltip={3.7}/>
+            </Card>
         </div>
     )
 }
